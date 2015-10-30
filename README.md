a3faa6f4b4acc4f4d79ab7f279a6f723548ac5d6
Daniel Schwiperich <d.schwiperich@cts-media.eu>
2015-10-30 13:08:31 +0100

    Added README development infos added gulp functionality for development purpose Updated gitignore

--- README.md
+++ README.md
@@ -2,3 +2,53 @@
 phpbb 3.1 Bridge for Contao 4 
 
 **not stable yet**
+
+## Development / Testing
+
+We provide a set of development tools if you like to contribute or modify the bridge
+
+### Docker
+
+This bundle comes with a pre configured docker container which gives you a working development 
+environment within minutes.
+You get a: 
+ - full equiped LAMP Server 
+ - contao/standard-edition
+ - already enabled bridge modul
+ - phpbb @TODO
+ - phpmyadmin
+
+1. Build the container: `docker build -t phpbbBridge:latest ./`
+
+2. You can now run the container:
+`docker run --name phpbb_bridge phpbb_bridge:latest /sbin/my_init --enable-insecure-key`
+
+3. The Container is now running. Access it via IP: `http://containerip/install.php`
+or via the host entry: `http://phpbbbridge.contao.local/install.php`
+
+
+#### Access your container
+We've ssh enabled for deployment and access.  
+You can add your own key or use the default key which is already enabled.  
+For development purpose using the *insecure* is perfectly fine as long as the container runs on you own machine.
+
+Download the key and put it where you like (right here or at ~/.ssh/)  
+```
+curl -o insecure_key -fSL https://github.com/phusion/baseimage-docker/raw/master/image/services/sshd/keys/insecure_key
+chmod 600 insecure_key
+```  
+Create a local gulp config (is ignored by git)  
+```
+touch gulp.config.js
+echo "docker.sftp.key='PATH_TO_KEY/insecure_key';" >> gulp.config.js 
+```  
+
+You can now use the gulp task `docker:watch` which automatically uploads all you changes made to the code (under src/).  
+For sure you can use normal SFTP and connect to you container if you like. The gulp thing is completely optional
+
+The gulp task will also start your container if stopped and modify your /etc/hosts if it's writable so you can access the container not 
+only via ip (which is shown on docker run or if you connect)
+
+#### gulp
+To use gulp you need to have node installed. If so, just call `npm install` and all dependencies will be loaded.
+You then can run `gulp docker:watch`
---
+++ gulpfile.js
@@ -0,0 +1,154 @@
+// Dependencies
+var gulp = require('gulp'),
+    gsftp = require('gulp-sftp'),
+    watch = require('gulp-watch');
+
+var exec = require('child_process').execSync;
+var fs = require('fs');
+
+// Config
+// Overwrite settings in local config file if needed: gulp.config.js
+var docker = {
+    domain: 'phpbbbridge.contao.local',
+    container: 'phpbb_bridge',
+    remotePath: '/var/www/share/phpbbbridge.contao.local/contao',
+    user: 'root'
+};
+docker.cmd = {
+    run: 'docker run -it --name '+docker.container+' phpbb_bridge:latest /sbin/my_init --enable-insecure-key',
+    start: 'docker start '+docker.container,
+    getIp: 'docker inspect --format  \'{{ .NetworkSettings.IPAddress }}\' '+docker.container
+};
+docker.sftp = {
+    host:docker.domain,
+    user:docker.user,
+    remotePath: docker.remotePath,
+    remotePathBridge: docker.remotePath + '/vendor/ctsmedia/contao-phpbb-bridge-bundle',
+    remotePathVendors: docker.remotePath + '/vendor',
+    passphrase: null
+};
+
+// src dirs
+var srcVendors = [
+        'vendor/?(contao/core-bundle/src)/**/*', // Some vendors, for debugging purposes
+        '!vendor/phpbb/phpbb/**/*', // not atm
+    ],
+    srcBridge = 'src/**/*';
+
+
+// Run this before all other tasks
+gulp.task('init', function (cb) {
+    // Load local Settings if exist
+    try {
+        if(fs.statSync('./gulp.config.js').isFile()){
+            console.log("Local Config File found.");
+            eval(fs.readFileSync('./gulp.config.js').toString());
+            console.log("File parsed. Overwriting Defaults.");
+        }
+    } catch(error){
+        if(error.code == 'ENOENT') {
+            console.log("No Local config file found.");
+        } else {
+            return cb(error);
+        }
+    }
+    cb();
+});
+
+// Look for docker container and start it
+gulp.task('docker:init', ['init'], function (cb) {
+
+    var containerRunning = false,  containerRunningTries = 0;
+
+    // Find Container
+    console.log('Looking for Container '+docker.container);
+    try {
+        exec('docker ps -a | grep -wc ' + docker.container);
+    } catch(error) {
+        console.log(error);
+        console.log("No Docker Container found. (was looking for "+docker.container+"). You have to run it by yourself:");
+        console.log(docker.cmd.run);
+        return cb(new Error("No Docker Container found. (was looking for "+docker.container+")."));
+    }
+    console.log("Container found. Testing if it started...");
+
+    // Start Container
+    while (containerRunning == false && containerRunningTries < 3) {
+        try {
+            exec('docker ps | grep -wc ' + docker.container);
+            console.log("Container is running");
+            containerRunning = true;
+        } catch(error) {
+            console.log("Container is not running. Trying to start. Try: #"+(containerRunningTries+1));
+            exec(docker.cmd.start);
+        }
+
+        containerRunningTries++;
+    }
+
+    // Setting hosts entry
+    console.log("Looking for IP Address: ");
+    var ip = exec(docker.cmd.getIp).toString('utf-8').replace(/(\r\n|\n|\r)/gm,"");
+    console.log(ip);
+    var isHostWritable = true;
+    fs.access('/etc/hosts', fs.W_OK, function(err){
+        if(err) isHostWritable = false;
+
+        if(isHostWritable == true) {
+            console.log("Adding to /etc/hosts");
+            exec('cp /etc/hosts ~/.hosts');
+            exec("sed -i '/"+docker.domain+"/d' ~/.hosts");
+            exec('echo "'+ip+' '+docker.domain+'" >> ~/.hosts');
+            exec('cat ~/.hosts > /etc/hosts');
+            exec('rm ~/.hosts');
+            console.log("host entry added");
+            console.log("You can now acces your container via: http://"+docker.domain);
+            console.log("On first run you've to install contao: http://"+docker.domain+"/install.php");
+
+        } else {
+            console.log("Hosts cannot be written. Do it by yourseld:");
+            console.log(ip + " " +docker.domain);
+        }
+    });
+
+    // Cleanup
+    if(containerRunning == false )
+        return cb(new Error("Container could not be started. Tried it "+containerRunningTries+" times"));
+
+    cb();
+
+});
+
+
+/*************************************************
+ * Watch tasks
+ ************************************************/
+// Docker Watcher
+// Uploads files automatically
+gulp.task('docker:watch', ['docker:init'], function () {
+
+    // Watch the Module dir and upload on changes to docker
+    watch(srcBridge, function(vynil){
+        var ftpData = docker.sftp;
+        ftpData.remotePath = ftpData.remotePathBridge;
+        console.log('Module File ' + vynil.path + ' was ' + vynil.event + ', running docker sftp...');
+        console.log(docker.sftp);
+        console.log("xxx");
+        gulp.src(vynil.path, {base: './'})
+            .pipe(gsftp(ftpData));
+    });
+
+    // Watch the Vendors dir and upload on changes to docker
+    watch(srcVendors, function(vynil){
+        console.log('Module File ' + vynil.path + ' was ' + vynil.event + ', running docker sftp...');
+        var ftpData = docker.sftp;
+        ftpData.remotePath = ftpData.remotePathVendors;
+        gulp.src(vynil.path, {base: './'})
+            .pipe(gsftp(ftpData));
+    });
+
+});
+
+
+
+
